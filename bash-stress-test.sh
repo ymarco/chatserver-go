dad221763b44629375fe9d219b705dffba8fc7c6
Yoav Marco <yoavm448@gmail.com>
2023-01-25 13:27:40 +0200

    bash-stress-test

---
+++ bash-stress-test.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PORT=4567
+MSGS_COUNT=$((1<<14))
+
+client1() {
+    echo $MSGS_COUNT\
+      | bb '(println "r")
+            (println "yoav")
+            (println "1234")
+            (Thread/sleep 2100) ; wait for client2 to login
+            (dotimes [_ (read)]
+              (println "msg"))
+            (Thread/sleep 13000); wait to receive messages' \
+      | go run . $PORT client\
+      | bb '(read-line) (read-line) ; connected, type r or l
+            (read-line) ; "Username:"
+            (read-line) ; "Password:"
+            (read-line) ; "Logged in as..."
+            (flush)
+            (read-line) ; ""
+            (println "client1: " (count (line-seq (java.io.BufferedReader. *in*))))'
+}
+client2() {
+    echo $MSGS_COUNT\
+      | bb '(println "r")
+            (println "nimrod")
+            (println "1234")
+            (Thread/sleep 2000) ; wait for client2 to login
+            (dotimes [_ (read)]
+              (println "msg"))
+            (Thread/sleep 13000); wait to receive messages' \
+      | go run . $PORT client \
+      | bb '(read-line) (read-line) ; connected, type r or l
+            (read-line) ; "Username:"
+            (read-line) ; "Password:"
+            (read-line) ; "Logged in as..."
+            (flush)
+            (read-line) ; ""
+            (println "client2: " (count (line-seq (java.io.BufferedReader. *in*))))'
+}
+go run . $PORT server&
+serverPID=$!
+sleep 0.2s
+
+client1&
+client1PID=$!
+
+client2&
+client2PID=$!
+
+wait $client1PID
+wait $client2PID
+
+kill -INT $serverPID
+echo "Killed server"
